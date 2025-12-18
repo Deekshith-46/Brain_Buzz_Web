2045ff372aa16926f7ea2af56fa3932bae6ad6cc
Deekshith-46 <tdeekshith46@gmail.com>
2025-12-18 12:04:41 +0530

    Errors

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ const orderRoutes = require('./routes/User/orderRoutes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
